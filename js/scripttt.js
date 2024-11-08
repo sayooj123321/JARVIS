70309ab02f61d372c8da222b9be48d9687ae5101
sayooj <152469369+sayooj123321@users.noreply.github.com>
2024-11-09 00:01:13 +0530

    Update scripttt.js

--- js/scripttt.js
+++ js/scripttt.js
@@ -45,7 +45,7 @@ recognition.onresult = async (event) => {
     console.log("Voice input received:", transcript);
 
      if (transcript.includes("what is your name")||transcript.includes("your name")||transcript.includes("name")) {
-        speakResponse("I am Jarvis.");
+        speakResponse("I am Milan.");
         document.getElementById("result").innerText = "i am jarvis";
     } else if (transcript.includes("who are you")) {
         speakResponse("I am a personal assistant, call me Jarvis.");
